65a848077af6b03782b7fdeeaeb8af9a86f3b856
maol <maol@nanofilm.com.cn>
2023-06-28 13:53:39 +0800

    feat: 引入 antd，首页添加head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,26 @@
-// import ThemeSwitch from "@/components/ThemeSwitch";
-import { useTheme } from "next-themes";
 import Link from "next/link";
+import { Button } from "antd";
+import Head from "next/head";
 
 export default function Home() {
-  
+
   return (
-    <div className="text-center text-gray-800 dark:text-gray-100 font-bold">
-      Home 
-      <div>
-        <Link href={'/chat'}>Chat</Link>
+    <>
+      <Head>
+        <title>AIGC</title>
+        <meta name="description" content="A free platform by Eatmorefishs." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="text-center text-gray-800 dark:text-gray-100 font-bold ">
+        Home
+        <div>
+          <Link href={'/chat'}>Chat</Link>
+        </div>
+
+        <Button type="primary" className="dark: bg-sky-600"> Button</Button>
       </div>
-
-    {/* <ThemeSwitch /> */}
-
-    </div>
+    </>
 
 
   )
