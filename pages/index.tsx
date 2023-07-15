dfc76a4c42694b666e2354ada7e62395d8adf1ae
gaohan <gaohan>
2023-07-15 14:05:16 +0800

    微调 首页卡片高度百分百

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="mx-auto max-w-2xl lg:max-w-none">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
             <Link href="/chat">
-              <div className="flex">
+              <div className="flex h-full">
                 <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
                   <h3>
                     <Image
@@ -41,7 +41,7 @@ export default function Home() {
               </div>
             </Link>
             <Link href="/ai">
-              <div className="flex">
+              <div className="flex h-full">
                 <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
                 <h3>
                   <Image
@@ -66,37 +66,37 @@ export default function Home() {
               </div>
             </Link>
             <Link href="/hot">
-            <div className="flex">
-              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-              <h3>
-                <Image
-                  src="/hot.svg" alt="AIGC Hot"
-                  width="64"
-                  height="64"
-                >
-                </Image>
-                </h3>
-                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
-                  <time className="font-semibold">2023</time>
-                  <span className="text-neutral-300" aria-hidden="true">|</span>
-                  <span>Hot</span>
-                </p>
-                <p className="mt-6 font-display text-xl font-semibold text-neutral-950">
-                  This Is The Hot List
-                </p>
-                <p className="mt-4 text-base text-neutral-600">
-                  Transform your text descriptions into stunning AI-generated artworks. Experience the fusion of AI and artistic expression as AIGC generates unique and captivating visuals tailored to your inputs.
-                </p>
-              </article>
-            </div>
+              <div className="flex h-full" >
+                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+                <h3>
+                  <Image
+                    src="/hot.svg" alt="AIGC Hot"
+                    width="64"
+                    height="64"
+                  >
+                  </Image>
+                  </h3>
+                  <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
+                    <time className="font-semibold">2023</time>
+                    <span className="text-neutral-300" aria-hidden="true">|</span>
+                    <span>Hot</span>
+                  </p>
+                  <p className="mt-6 font-display text-xl font-semibold text-neutral-950">
+                    This Is The Hot List
+                  </p>
+                  <p className="mt-4 text-base text-neutral-600">
+                    Transform your text descriptions into stunning AI-generated artworks. Experience the fusion of AI and artistic expression as AIGC generates unique and captivating visuals tailored to your inputs.
+                  </p>
+                </article>
+              </div>
             </Link>
 
             <Link href="">
-            <div className="flex">
-              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-                2303
-              </article>
-            </div>
+              <div className="flex h-full">
+                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+                  2303
+                </article>
+              </div>
             </Link>
           </div>
         </div>
