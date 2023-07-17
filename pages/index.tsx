a075d7aa59206e20206cf4606a5fdd164748ed55
gaohan <gaohan>
2023-07-17 21:37:04 +0800

    首页卡片1

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,88 +16,76 @@ export default function Home() {
         <div className="mx-auto max-w-2xl lg:max-w-none">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
             <Link href="/chat">
-              <div className="flex h-full">
-                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-                  <h3>
+              <div className="flex  bg-[#059669] rounded-3xl">
+                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition sm:p-4">
+                  <div className="justify-between flex">
+                    <div className="flex">
+                      <span className="text-[24px] text-white mr-3">Chat</span>
+                      <Image
+                        src="/search.svg" alt="AIGC Chat"
+                        width="15"
+                        height="15"
+                      >
+                      </Image>
+                    </div>
                     <Image
-                      src="/chat.svg" alt="AIGC Chat"
-                      width="64"
-                      height="64"
+                      src="/r.svg" alt="AIGC Chat"
+                      width="7"
+                      height="14"
                     >
                     </Image>
-                  </h3>
-                  <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
-                    <time className="font-semibold">2023</time>
-                    <span className="text-neutral-300" aria-hidden="true">|</span>
-                    <span>ChatGPT Chatbot</span>
-                  </p>
-                  <p className="mt-6 font-display text-xl font-semibold text-neutral-950">
-                    Intelligent Conversations
-                  </p>
-                  <p className="mt-4 text-base text-neutral-600">
-                    Engage in natural language conversations with our advanced ChatGPT chatbot. Get personalized responses, suggestions, and even entertainment, all through interactive and human-like interactions.
-                  </p>
-                </article>
-              </div>
-            </Link>
-            <Link href="/ai">
-              <div className="flex h-full">
-                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-                <h3>
-                  <Image
-                    src="/paint.svg" alt="AIGC Hot"
-                    width="64"
-                    height="64"
-                  >
-                  </Image>
-                </h3>
-                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
-                  <time className="font-semibold">2023</time>
-                  <span className="text-neutral-300" aria-hidden="true">|</span>
-                  <span>AI Painting</span>
-                </p>
-                <p className="mt-6 font-display text-xl font-semibold text-neutral-950">
-                  Unleash Your Creativity
-                </p>
-                <p className="mt-4 text-base text-neutral-600">
-                  Transform your text descriptions into stunning AI-generated artworks. Experience the fusion of AI and artistic expression as AIGC generates unique and captivating visuals tailored to your inputs.
-                </p>
-                </article>
-              </div>
-            </Link>
-            <Link href="/hot">
-              <div className="flex h-full" >
-                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-                <h3>
-                  <Image
-                    src="/hot.svg" alt="AIGC Hot"
-                    width="64"
-                    height="64"
-                  >
-                  </Image>
-                  </h3>
-                  <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
-                    <time className="font-semibold">2023</time>
-                    <span className="text-neutral-300" aria-hidden="true">|</span>
-                    <span>Hot</span>
-                  </p>
-                  <p className="mt-6 font-display text-xl font-semibold text-neutral-950">
-                    This Is The Hot List
-                  </p>
-                  <p className="mt-4 text-base text-neutral-600">
-                    Transform your text descriptions into stunning AI-generated artworks. Experience the fusion of AI and artistic expression as AIGC generates unique and captivating visuals tailored to your inputs.
-                  </p>
+                  </div>
+                  <div className="">
+                    <div className="one justify-items-end grid">
+                      <Image
+                        src="/me.svg" alt="me"
+                        width="44"
+                        height="44"
+                      >
+                      </Image>
+                      <div className="text w-[298px] h-[42px] my-2.5 bg-[#FFFFFF]/75 rounded-[8px]" ></div>
+                    </div>
+                    <div className="two">
+                    <Image
+                      src="/c.svg" alt="c"
+                      width="44"
+                      height="44"
+                    >
+                    </Image>
+                    <div className="text max-w-[298px] h-[42px] my-2.5 bg-[#FFFFFF]/75 rounded-[8px] mb-[50px]" ></div>
+                    </div>
+                  </div>
                 </article>
               </div>
             </Link>
 
-            <Link href="">
-              <div className="flex h-full">
-                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
-                  2303
+            <Link href="/hot">
+              <div className="flex  bg-[#F2B32D] rounded-3xl" >
+                <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition  sm:p-4">
+                  <div className="justify-between flex">
+                    <div className="flex">
+                      <span className="text-[24px] text-white mr-3">热搜榜</span>
+                      <Image
+                        src="/search.svg" alt="AIGC hot"
+                        width="15"
+                        height="15"
+                      >
+                      </Image>
+                    </div>
+                    <Image
+                      src="/r.svg" alt="AIGC hot"
+                      width="7"
+                      height="14"
+                    >
+                    </Image>
+                  </div>
+                  <div className="list">
+                    <div className=""></div>
+                  </div>
                 </article>
               </div>
             </Link>
+
           </div>
         </div>
       </main>
